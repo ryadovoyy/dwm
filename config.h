23dc0c33445db177ba9e21b347749a5989cd2eb2
ryadovoyy <holstinindvs@gmail.com>
2021-10-03 01:50:18 +0700

    Change font to Roboto Mono

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 5;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Roboto Mono:size=12:antialias=true" };
+static const char dmenufont[]       = "Roboto Mono:size=12:antialias=true";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
