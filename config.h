71b089de883e9cc55eaa857c56b79298c47734a0
ryadovoyy <holstinindvs@gmail.com>
2021-11-18 16:41:31 +0700

    Add several tag rules

--- config.h
+++ config.h
@@ -32,9 +32,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class              instance    title       tags mask     isfloating   monitor */
+	{ "Brave-browser",    NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "TelegramDesktop",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "discord",          NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
