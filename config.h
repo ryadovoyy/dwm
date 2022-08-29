a40ccad58ceb653b5e4caa2e519b72854de9d03e
ryadovoyy <holstinindvs@gmail.com>
2022-08-29 20:29:22 +0700

    Add the emacs and st rules

--- config.h
+++ config.h
@@ -34,6 +34,8 @@ static const Rule rules[] = {
 	 */
 	/* class              instance    title       tags mask     isfloating   monitor */
 	{ "Brave-browser",    NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "St",               NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Emacs",            NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "TelegramDesktop",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
@@ -65,6 +67,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-h", dmenulh, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *emacscmd[] = { "emacsclient", "-c", NULL };
 static const char *mutevol[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 static const char *flameshot[] = { "flameshot", "gui", NULL };
 
@@ -72,6 +75,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = emacscmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
