2fef0a3a90b0f71274778674db365165bc93688a
ryadovoyy <holstinindvs@gmail.com>
2022-10-26 05:19:28 +0700

    Increase the gap size

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const int borderpxbar        = 0;        /* border pixel of statusbar */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
+static const Gap default_gap        = {.isgap = 1, .realgap = 15, .gappx = 15};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
